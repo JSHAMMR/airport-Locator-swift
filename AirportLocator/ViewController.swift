4b87d0dcd776faae339917ca0311054f3fbb9d19
Jamil Shamar <jameelshammr@gmail.com>
2019-11-25 21:16:48 +0800

    enable google places

--- AirportLocator/ViewController.swift
+++ AirportLocator/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class ViewController: UIViewController {
+    var placesClient: GMSPlacesClient!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        placesClient = GMSPlacesClient.shared()
+
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func getCurrentPlace(_ sender: UIButton) {
 
+       placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+         if let error = error {
+           print("Current Place error: \(error.localizedDescription)")
+           return
+         }
+
+         self.nameLabel.text = "No current place"
+         self.addressLabel.text = ""
+
+         if let placeLikelihoodList = placeLikelihoodList {
+           let place = placeLikelihoodList.likelihoods.first?.place
+           if let place = place {
+             self.nameLabel.text = place.name
+             self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+               .joined(separator: "\n")
+           }
+         }
+       })
+     }
 }
 
